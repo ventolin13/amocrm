88fe01a7d1e52207044f1f7740496815ff837e28
root <root@s94819.nocix.net>
2016-10-02 20:16:24 -0500

    2 day preparing

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# amocrm
-AmoCRM test problem
+MongoDB references
+==================
+
+https://habrahabr.ru/company/latera/blog/280196/
+http://eax.me/mongodb/
+https://habrahabr.ru/post/240405/ (Hadoop)
+https://habrahabr.ru/post/217393/ (Шардинг)
+https://docs.mongodb.com/manual/reference/ulimit/#recommended-settings (Настройки)
+http://docs.mongoengine.org/
+
+OFF (UPD: работа с кастомным полем в тестовом задании)
+
+Установка на Centos7
+====================
+
+echo "[mongodb-org-3.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.2.asc
+" >> /etc/yum.repos.d/mongodb-org-3.2.repo
+yum install -y mongodb-org
+
+nano /etc/mongod.conf
+<<dbPath /var/data
+chown -R mongod:mongod /var/data/
+
+https://docs.mongodb.com/manual/tutorial/transparent-huge-pages/#configure-thp-tuned
+and
+nano /etc/default/grub
+<<GRUB_CMDLINE_LINUX_DEFAULT="transparent_hugepage=never"
+update-grub
+https://docs.mongodb.com/manual/reference/ulimit/
+
+Базовая настройка Mongo
+=======================
+https://docs.mongodb.com/manual/tutorial/enable-authentication/
+https://docs.mongodb.com/manual/tutorial/manage-users-and-roles/
+http://stackoverflow.com/questions/35341670/how-can-i-remove-user-from-my-mongodb-database
+
+use admin
+db.createUser(
+  {
+    user: "admin",
+    pwd: "adminmongo",
+    roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]
+  }
+)
+nano /etc/mongod.conf
+<<security:
+<<authorization: enabled
+
+use laptopshop
+db.createUser(
+  {
+    user: "pit",
+    pwd: "userpass",
+    roles: [ { role: "readWrite", db: "laptopshop" },
+             { role: "read", db: "reporting" } ]
+  }
+)
+
+Mongoengine
+===========
+pip install mongoengine
+
+from pymongo import MongoClient
+
+client = MongoClient('localhost:27017')   
+client.admin.authenticate('admin', 'adminmongo')
+client.testdb.add_user('newTestUser', 'Test123', roles=[{'role':'readWrite','db':'testdb'}])
+
+https://habrahabr.ru/post/134590/
+https://habrahabr.ru/sandbox/26218/
+http://api.mongodb.com/python/current/api/pymongo/index.html
+https://docs.mongodb.com/manual/tutorial/query-documents/
+
--- main.py
+++ main.py
@@ -1,24 +1,127 @@
 from flask import Flask, request, render_template, redirect
-from httplib2 import Http
 from urllib.parse import urlencode
 import re
 import requests
+import mongoengine as me
 
 try:
-	import json
-except ImportError:
 	import simplejson as json
+except ImportError:
+	import json
 
 
 AMO_URL = "https://new57dfff397ffd3.amocrm.ru"
 AMO_LOGIN = "venya1178@gmail.com"
 AMO_KEY = "380d97e3b222b0259695e097db5ee3a2"
+CUSTOM_FIELDS = {
+	'companies': [
+			{
+			'enums': {'2478932': 'WORKDD', '2478938': 'HOME', '2478934': 'MOB', '2478936': 'FAX', '2478940': 'OTHER', '2478930': 'WORK'},
+			'name': 'Телефон',
+			'code': 'PHONE',
+			'multiple': 'Y',
+			'sort': 4,
+			'disabled': '0',
+			'type_id': '8',
+			'id': '1026278'
+		},{
+			'enums': {'2478946': 'OTHER', '2478942': 'WORK', '2478944': 'PRIV'},
+			'name': 'Email',
+			'code': 'EMAIL',
+			'multiple': 'Y',
+			'sort': 6,
+			'disabled': '0',
+			'type_id': '8',
+			'id': '1026280'
+		},{
+			'name': 'Web',
+			'code': 'WEB',
+			'multiple': 'N',
+			'sort': 8,
+			'disabled': '0',
+			'type_id': '7',
+			'id': '1026282'
+		},{
+			'name': 'Адрес',
+			'code': 'ADDRESS',
+			'multiple': 'N',
+			'sort': 12,
+			'disabled': '0',
+			'type_id': '9',
+			'id': '1026286'
+		}
+	], 
+	'contacts': [
+			{
+			'name': 'Должность',
+			'code': 'POSITION',
+			'multiple': 'N',
+			'sort': 2,
+			'disabled': '0',
+			'type_id': '1',
+			'id': '1026276'
+		},{
+			'enums':{'2478932': 'WORKDD', '2478938': 'HOME', '2478934': 'MOB', '2478936': 'FAX', '2478940': 'OTHER', '2478930': 'WORK'},
+			'name': 'Телефон',
+			'code': 'PHONE',
+			'multiple': 'Y',
+			'sort': 4,
+			'disabled': '0',
+			'type_id': '8',
+			'id': '1026278'
+		},{
+			'enums': {'2478946': 'OTHER', '2478942': 'WORK', '2478944': 'PRIV'},
+			'name': 'Email',
+			'code': 'EMAIL',
+			'multiple': 'Y',
+			'sort': 6,
+			'disabled': '0',
+			'type_id': '8',
+			'id': '1026280'
+		},{
+			'enums': {'2478950': 'ICQ', '2478954': 'GTALK', '2478952': 'JABBER', '2478956': 'MSN', '2478958': 'OTHER', '2478948': 'SKYPE'},
+			'name': 'Мгн. сообщения',
+			'code': 'IM',
+			'multiple': 'Y',
+			'sort': 10,
+			'disabled': '0',
+			'type_id': '8',
+			'id': '1026284'
+		}
+	]
+}
+
+CUSTOM_COMPANIES_IDS = {
+	'PHONE' : 1026278, 
+	'EMAIL' : 1026280,
+	'WEB' : 1026282,
+	'ADDRESS' : 1026286,
+}
+
+CUSTOM_CONTACTS_IDS = {
+	'POSITION' : 1026276,
+	'PHONE' : 1026278, 
+	'EMAIL' : 1026280, 
+	'IM' : 1026284,
+}
+
 app = Flask(__name__)
+
+from werkzeug.contrib.fixers import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
 app.debug = True
+
+@app.route("/get_my_ip", methods=["GET"])
+def get_my_ip():
+	app.logger.info(request.__dict__)
+	ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+	return json.dumps({'ip': str(ip)}), 200
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+	app.logger.info(request.__dict__)
+	return render_template('home.html', CUSTOM_CONTACTS_IDS=CUSTOM_CONTACTS_IDS)
 	
 @app.route('/contacts', methods=['GET', 'POST'])
 def contacts():
@@ -29,6 +132,7 @@ def contacts():
 	if request.method == 'POST':
 		name = request.form["name"]
 		company_name = request.form["company_name"]
+		custom_id = request.form["custom_id"]
 		custom_str = request.form["custom_str"]
 		
 		s = requests.session()
@@ -54,10 +158,11 @@ def contacts():
 			"company_name" : company_name, 
 			"custom_fields" : [
 				{
-					"id" : 45860529,
+					"id" : CUSTOM_CONTACTS_IDS[custom_id],
 					"values" : [
 						{
-						"value" : 55555, 
+						"value" : custom_str, 
+						"enum" : "OTHER"
 						}
 					]			
 				},
